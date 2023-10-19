c89bdd9142e70318e8fed929da5fe2c7d1cb183f
Canelita <ing69chipmunks@hotmail.com>
2023-10-19 16:22:37 -0300

    AAAAAAAAAA

--- app/src/main/java/com/example/mapita/GoogleMaps.java
+++ app/src/main/java/com/example/mapita/GoogleMaps.java
@@ -1,14 +1,50 @@
 package com.example.mapita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class GoogleMaps extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMaps extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapita);
+        mapFragment.getMapAsync(this);
+
+        btnregreso.SetOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        MainActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        GoogleMap mMap = googleMap;
+
+        double lon1 = getIntent() .getDoubleExtra("lo1", 0.0);
+        double lat1 = getIntent() .getDoubleExtra("lat1", 0.0);
+
+        LatLng puntito1 = new LatLng(lon1, lat1);
+        mMap.addMarker(new MarkerOptions()
+
     }
 }
