471d7a3ae9b545034ac434c0df3d199f604eaef2
Canelita <ing69chipmunks@hotmail.com>
2023-10-19 15:27:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mapita/GoogleMaps.java
@@ -0,0 +1,14 @@
+package com.example.mapita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoogleMaps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps);
+    }
+}
