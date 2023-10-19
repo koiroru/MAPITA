fbe02348528777f4b91f4c05645124ba82a7ea16
Canelita <ing69chipmunks@hotmail.com>
2023-10-19 16:38:15 -0300

    JARVIS LLAMA A DIOS

--- app/src/main/java/com/example/mapita/GoogleMaps.java
+++ app/src/main/java/com/example/mapita/GoogleMaps.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -43,7 +44,19 @@ public class GoogleMaps extends AppCompatActivity implements OnMapReadyCallback{
         double lon1 = getIntent() .getDoubleExtra("lo1", 0.0);
         double lat1 = getIntent() .getDoubleExtra("lat1", 0.0);
 
+        double lon2 = getIntent() .getDoubleExtra("lo2", 0.0);
+        double lat2 = getIntent() .getDoubleExtra("lat2", 0.0);
+
+        double lon3 = getIntent() .getDoubleExtra("lo3", 0.0);
+        double lat3 = getIntent() .getDoubleExtra("lat3", 0.0);
+
         LatLng puntito1 = new LatLng(lon1, lat1);
+        mMap.addMarker(new MarkerOptions()
+
+        LatLng puntito2 = new LatLng(lon2, lat2);
+        mMap.addMarker(new MarkerOptions()
+
+        LatLng puntito3 = new LatLng(lon3, lat3);
         mMap.addMarker(new MarkerOptions()
 
     }
